d07d3b6e7563efc59b4e03a51a0139f8a33b1bd6
wrealaero <clanwarobi@gmail.com>
2025-04-02 18:41:34 -0500

    Update whitelist_manager.lua

--- whitelist_manager.lua
+++ whitelist_manager.lua
@@ -1,13 +1,10 @@
 local WhitelistManager = {}
 
--- Configuration - Your whitelist URL
 local whitelist_url = "https://raw.githubusercontent.com/wrealaero/whitelistcheck/refs/heads/main/newwhitlist.json"
 
--- Fetch whitelist from GitHub
 function WhitelistManager:fetchWhitelist()
     local HttpService = game:GetService("HttpService")
-    
-    -- Try to get the whitelist with retries
+
     local retries = 3
     local success, response
     
@@ -34,28 +31,22 @@ function WhitelistManager:fetchWhitelist()
     return {}
 end
 
--- Check if a user is whitelisted
 function WhitelistManager:isWhitelisted(userId)
     local whitelist = self:fetchWhitelist()
-    
-    -- Basic check - is the UserID in the whitelist?
+
     if not whitelist or not whitelist[userId] then
         return false, nil
     end
     
     local userEntry = whitelist[userId]
     
-    -- Check if it's a table (new format) or string (old format)
     if type(userEntry) == "table" then
-        -- Check if banned
         if userEntry.banned then
             return false, nil
         end
         
-        -- Return user tier
         return true, userEntry.tier or "default"
     else
-        -- Old format - just a string or boolean
         return true, "default"
     end
 end
